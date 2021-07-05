0b10f4f5054e49454dc52a93ce3c9b92c07fe0aa
Xue Jiao <jiao.xue@shopee.com>
2021-07-05 15:49:57 +0800

    More test cases for Args.Parse

--- args_test.go
+++ args_test.go
@@ -46,6 +46,39 @@ func TestArgsParse(t *testing.T) {
 
     // empty args
     testArgsParse(t, &a, "", 0, "foo=", "bar=", "=")
+
+    // arg without value
+    testArgsParse(t, &a, "foo1", 1, "foo=", "bar=", "=")
+
+    // arg without value, but with equal sign
+    testArgsParse(t, &a, "foo2=", 1, "foo=", "bar=", "=")
+
+    // arg with value
+    testArgsParse(t, &a, "foo3=bar1", 1, "foo3=bar1", "bar=", "=")
+
+    // empty key
+    testArgsParse(t, &a, "=bar2", 1, "foo=", "=bar2", "bar2=")
+
+    // missing kv
+    testArgsParse(t, &a, "&&&&", 0, "foo=", "bar=", "=")
+
+    // multiple value with the same key
+    testArgsParse(t, &a, "x=1&x=2&x=3", 3, "x=1")
+
+    // multiple args without values
+    testArgsParse(t, &a, "&&a&&b&&bar&baz", 4, "a=", "b=", "bar=", "baz=")
+
+    // values with '='
+    testArgsParse(t, &a, "zz=1&k=v=v=a=a=s", 2, "k=v=v=a=a=s", "zz=1")
+
+    // mixed '=' and '&'
+    testArgsParse(t, &a, "sss&z=dsf=&df", 3, "sss=", "z=dsf=", "df=")
+
+    // encoded args
+    testArgsParse(t, &a, "%E8%96%9B=%E8%9B%9F", 1, "薛=蛟")
+
+    // invalid percent encoding
+    testArgsParse(t, &a, "f%=x&qw%z=d%0k%20p&%%20=%%%20x", 3, "f%=x", "qw%z=d%0k p", "% =%% x")
 }
 
 func testArgsParse(t *testing.T, a *Args, s string, expectedLen int, expectedArgs ...string) {
