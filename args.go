69b288febef107cd50ce9dd6e6d69af486062600
Xue Jiao <jiao.xue@shopee.com>
2021-07-05 16:11:44 +0800

    Test Args.Get Set Del

--- args.go
+++ args.go
@@ -1,137 +1,150 @@
 package fasthttp
 
 import (
-    "bytes"
+	"bytes"
 )
 
 type Args struct {
-    args []argsKV
-    buf []byte
+	args []argsKV
+	buf  []byte
 }
 
 type argsKV struct {
-    key []byte
-    value []byte
+	key   []byte
+	value []byte
 }
 
 func (a *Args) Clear() {
-    a.args = a.args[:0]
+	a.args = a.args[:0]
 }
 
 func (a *Args) Len() int {
-    return len(a.args)
+	return len(a.args)
 }
 
 func (a *Args) Set(key, value string) {
-    a.buf = CopyBytesStr(a.buf, value)
-    a.SetBytes(key, a.buf)
+	a.buf = CopyBytesStr(a.buf, value)
+	a.SetBytes(key, a.buf)
 }
 
 // Why not turn the key into []byte?
 func (a *Args) SetBytes(key string, value []byte) {
-    n := len(a.args)
-    // in case the key exists
-    for i := 0; i < n; i++ {
-        kv := &a.args[i]
-        if EqualBytesStr(kv.key, key) {
-            kv.value = append(kv.value[:0], value...)
-            return
-        }
-    }
+	n := len(a.args)
+	// in case the key exists
+	for i := 0; i < n; i++ {
+		kv := &a.args[i]
+		if EqualBytesStr(kv.key, key) {
+			kv.value = append(kv.value[:0], value...)
+			return
+		}
+	}
 
-    if cap(a.args) > n {
-        a.args = a.args[:n+1]
-        kv := &a.args[n]
-        kv.key = CopyBytesStr(kv.key, key)
-        kv.value = append(kv.value[:0], value...)
-        return
-    }
+	if cap(a.args) > n {
+		a.args = a.args[:n+1]
+		kv := &a.args[n]
+		kv.key = CopyBytesStr(kv.key, key)
+		kv.value = append(kv.value[:0], value...)
+		return
+	}
 
-    var kv argsKV
-    kv.key = CopyBytesStr(kv.key, key)
-    kv.value = append(kv.value, value...)
-    a.args = append(a.args, kv)
+	var kv argsKV
+	kv.key = CopyBytesStr(kv.key, key)
+	kv.value = append(kv.value, value...)
+	a.args = append(a.args, kv)
 }
 
 // Warning: each call allocates memory for returned string.
 func (a *Args) Get(key string) string {
-    return string(a.Peek(key))
+	return string(a.Peek(key))
 }
 
 func (a *Args) GetBytes(dst []byte, key string) []byte {
-    value := a.Peek(key)
-    return append(dst[:0], value...)
+	value := a.Peek(key)
+	return append(dst[:0], value...)
 }
 
 func (a *Args) Peek(key string) []byte {
-    for i, n := 0, len(a.args); i < n; i++ {
-        kv := &a.args[i]
-        if EqualBytesStr(kv.key, key) {
-            return kv.value
-        }
-    }
-    return nil
+	for i, n := 0, len(a.args); i < n; i++ {
+		kv := &a.args[i]
+		if EqualBytesStr(kv.key, key) {
+			return kv.value
+		}
+	}
+	return nil
 }
 
 func (a *Args) Has(key string) bool {
-    return a.Peek(key) != nil
+	return a.Peek(key) != nil
+}
+
+func (a *Args) Del(key string) {
+	for i, n := 0, len(a.args); i < n; i++ {
+		kv := &a.args[i]
+		if EqualBytesStr(kv.key, key) {
+			tmp := *kv
+			copy(a.args[i:], a.args[i+1:])
+			a.args[n-1] = tmp
+			a.args = a.args[:n-1]
+			return
+		}
+	}
 }
 
 // Get query string
 func (a *Args) String() string {
-    a.buf = a.AppendBytes(a.buf[:0])
-    return string(a.buf)
+	a.buf = a.AppendBytes(a.buf[:0])
+	return string(a.buf)
 }
 
 // Query string in []byte
 func (a *Args) AppendBytes(dst []byte) []byte {
-    for i, n := 0, len(a.args); i < n; i++ {
-        kv := &a.args[i]
-        dst = appendQuotedArg(dst, kv.key)
-        if len(kv.value) > 0 {
-            dst = append(dst, '=')
-            dst = appendQuotedArg(dst, kv.value)
-        }
-        if i+1 < n {
-            dst = append(dst, '&')
-        }
-    }
-    return dst
+	for i, n := 0, len(a.args); i < n; i++ {
+		kv := &a.args[i]
+		dst = appendQuotedArg(dst, kv.key)
+		if len(kv.value) > 0 {
+			dst = append(dst, '=')
+			dst = appendQuotedArg(dst, kv.value)
+		}
+		if i+1 < n {
+			dst = append(dst, '&')
+		}
+	}
+	return dst
 }
 
 // From query string
 func (a *Args) Parse(s string) {
-    a.buf = CopyBytesStr(a.buf, s)
-    a.ParseBytes(a.buf)
+	a.buf = CopyBytesStr(a.buf, s)
+	a.ParseBytes(a.buf)
 }
 
 // From query string in []byte
 func (a *Args) ParseBytes(b []byte) {
-    a.Clear()
-    var p argsParser
-    p.Init(b)
+	a.Clear()
+	var p argsParser
+	p.Init(b)
 
-    n := cap(a.args)
-    a.args = a.args[:n]
+	n := cap(a.args)
+	a.args = a.args[:n]
 
-    // Remove non-exists keys
-    for i := 0; i < n; i++ {
-        kv := &a.args[i]
-        if !p.Next(kv) {
-            for j := 0; j < i; j++ {
-            }
-            a.args = a.args[:i]
-            return
-        }
-    }
+	// Remove non-exists keys
+	for i := 0; i < n; i++ {
+		kv := &a.args[i]
+		if !p.Next(kv) {
+			for j := 0; j < i; j++ {
+			}
+			a.args = a.args[:i]
+			return
+		}
+	}
 
-    for {
-        var kv argsKV
-        if !p.Next(&kv) {
-            return
-        }
-        a.args = append(a.args, kv)
-    }
+	for {
+		var kv argsKV
+		if !p.Next(&kv) {
+			return
+		}
+		a.args = append(a.args, kv)
+	}
 }
 
 //////////////////////////////////////////////////
@@ -140,24 +153,24 @@ func (a *Args) ParseBytes(b []byte) {
 
 // Copy string into a byte array
 func CopyBytesStr(dst []byte, src string) []byte {
-    dst = dst[:0]
-    for i, n := 0, len(src); i < n; i++ {
-        dst = append(dst, src[i])
-    }
-    return dst
+	dst = dst[:0]
+	for i, n := 0, len(src); i < n; i++ {
+		dst = append(dst, src[i])
+	}
+	return dst
 }
 
 // check if a string equal a byte array
 func EqualBytesStr(b []byte, s string) bool {
-    if len(s) != len(b) {
-        return false
-    }
-    for i, n := 0, len(s); i<n; i++ {
-        if s[i] != b[i] {
-            return false
-        }
-    }
-    return true
+	if len(s) != len(b) {
+		return false
+	}
+	for i, n := 0, len(s); i < n; i++ {
+		if s[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 //////////////////////////////////////////////////
@@ -165,62 +178,62 @@ func EqualBytesStr(b []byte, s string) bool {
 //////////////////////////////////////////////////
 
 func appendQuotedArg(dst, v []byte) []byte {
-    for _, c := range v {
-        if c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '/' {
-            dst = append(dst, c)
-        } else {
-            dst = append(dst, '%', hexChar(c>>4), hexChar(c&15))
-        }
-    }
-    return dst
+	for _, c := range v {
+		if c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '/' {
+			dst = append(dst, c)
+		} else {
+			dst = append(dst, '%', hexChar(c>>4), hexChar(c&15))
+		}
+	}
+	return dst
 }
 
 func hexChar(c byte) byte {
-    if c < 10 {
-        return '0' + c
-    }
-    return c - 10 + 'A'
+	if c < 10 {
+		return '0' + c
+	}
+	return c - 10 + 'A'
 }
 
 func unhex(c byte) int {
-    if c >= '0' && c <= '9' {
-        return int(c - '0')
-    }
-    if c >= 'a' && c <= 'f' {
-        return 10 + int(c-'a')
-    }
-    if c >= 'A' && c <= 'F' {
-        return 10 + int(c-'A')
-    }
-    return -1
+	if c >= '0' && c <= '9' {
+		return int(c - '0')
+	}
+	if c >= 'a' && c <= 'f' {
+		return 10 + int(c-'a')
+	}
+	if c >= 'A' && c <= 'F' {
+		return 10 + int(c-'A')
+	}
+	return -1
 }
 
 func decodeArg(dst, src []byte, decodePlus bool) []byte {
-    for i, n := 0, len(src); i < n; i++ {
-        c := src[i]
-        switch c {
-        case '+':
-            if decodePlus {
-                c = ' '
-            }
-            dst = append(dst, c)
-        case '%':
-            if i+2 >= n {
-                return append(dst, src[i:]...)
-            }
-            x1 := unhex(src[i+1])
-            x2 := unhex(src[i+2])
-            if x1 < 0 || x2 < 0 {
-                dst = append(dst, c)
-            } else {
-                dst = append(dst, byte(x1<<4|x2))
-                i += 2
-            }
-        default:
-            dst = append(dst, c)
-        }
-    }
-    return dst
+	for i, n := 0, len(src); i < n; i++ {
+		c := src[i]
+		switch c {
+		case '+':
+			if decodePlus {
+				c = ' '
+			}
+			dst = append(dst, c)
+		case '%':
+			if i+2 >= n {
+				return append(dst, src[i:]...)
+			}
+			x1 := unhex(src[i+1])
+			x2 := unhex(src[i+2])
+			if x1 < 0 || x2 < 0 {
+				dst = append(dst, c)
+			} else {
+				dst = append(dst, byte(x1<<4|x2))
+				i += 2
+			}
+		default:
+			dst = append(dst, c)
+		}
+	}
+	return dst
 }
 
 //////////////////////////////////////////////////
@@ -228,53 +241,53 @@ func decodeArg(dst, src []byte, decodePlus bool) []byte {
 //////////////////////////////////////////////////
 
 type argsParser struct {
-    b []byte
+	b []byte
 }
 
 func (p *argsParser) Init(buf []byte) {
-    p.b = buf
+	p.b = buf
 }
 
 // Fill in the content, or return false
 func (p *argsParser) Next(kv *argsKV) bool {
-    for {
-        if !p.next(kv) {
-            return false
-        }
-        if len(kv.key) > 0 || len(kv.value) > 0 {
-            if kv.key == nil {
-                kv.key = []byte{}
-            }
-            if kv.value == nil {
-                kv.value = []byte{}
-            }
-            return true
-        }
-    }
+	for {
+		if !p.next(kv) {
+			return false
+		}
+		if len(kv.key) > 0 || len(kv.value) > 0 {
+			if kv.key == nil {
+				kv.key = []byte{}
+			}
+			if kv.value == nil {
+				kv.value = []byte{}
+			}
+			return true
+		}
+	}
 }
 
 func (p *argsParser) next(kv *argsKV) bool {
-    if len(p.b) == 0 {
-        return false
-    }
+	if len(p.b) == 0 {
+		return false
+	}
 
-    n := bytes.IndexByte(p.b, '&')
-    var b []byte
-    if n < 0 {
-        b = p.b
-        p.b = p.b[len(p.b):]
-    } else {
-        b = p.b[:n]
-        p.b = p.b[n+1:]
-    }
+	n := bytes.IndexByte(p.b, '&')
+	var b []byte
+	if n < 0 {
+		b = p.b
+		p.b = p.b[len(p.b):]
+	} else {
+		b = p.b[:n]
+		p.b = p.b[n+1:]
+	}
 
-    n = bytes.IndexByte(b, '=')
-    if n < 0 {
-        kv.key = decodeArg(kv.key[:0], b, true)
-        kv.value = kv.value[:0]
-    } else {
-        kv.key = decodeArg(kv.key[:0], b[:n], true)
-        kv.value = decodeArg(kv.value[:0], b[n+1:], true)
-    }
-    return true
+	n = bytes.IndexByte(b, '=')
+	if n < 0 {
+		kv.key = decodeArg(kv.key[:0], b, true)
+		kv.value = kv.value[:0]
+	} else {
+		kv.key = decodeArg(kv.key[:0], b[:n], true)
+		kv.value = decodeArg(kv.value[:0], b[n+1:], true)
+	}
+	return true
 }
--- args_test.go
+++ args_test.go
@@ -1,137 +1,202 @@
 package fasthttp
 
 import (
-    "testing"
-    "strings"
+	"fmt"
+	"strings"
+	"testing"
 )
 
 func TestArgsStringCompose(t *testing.T) {
-    var a Args
-    a.Set("foo", "bar")
-    a.Set("aa", "bbb")
-    a.Set("薛", "蛟")
-    a.Set("", "xxxx")
-    a.Set("cvx", "")
+	var a Args
+	a.Set("foo", "bar")
+	a.Set("aa", "bbb")
+	a.Set("薛", "蛟")
+	a.Set("", "xxxx")
+	a.Set("cvx", "")
 
-    expectedS := "foo=bar&aa=bbb&%E8%96%9B=%E8%9B%9F&=xxxx&cvx"
+	expectedS := "foo=bar&aa=bbb&%E8%96%9B=%E8%9B%9F&=xxxx&cvx"
 
-    s := a.String()
-    if s != expectedS {
-        t.Fatalf("Unexpected string %q. Expected %q", s, expectedS)
-    }
+	s := a.String()
+	if s != expectedS {
+		t.Fatalf("Unexpected string %q. Expected %q", s, expectedS)
+	}
 }
 
 func TestArgsString(t *testing.T) {
-    var a Args
+	var a Args
 
-    testArgsString(t, &a, "")
-    testArgsString(t, &a, "foobar")
-    testArgsString(t, &a, "foo=bar")
-    testArgsString(t, &a, "foo=bar&baz=sss")
-    testArgsString(t, &a, "")
-    testArgsString(t, &a, "foo=bar&aa=bbb&%E8%96%9B=%E8%9B%9F&=xxxx&cvx")
-    testArgsString(t, &a, "=asdfsdf")
+	testArgsString(t, &a, "")
+	testArgsString(t, &a, "foobar")
+	testArgsString(t, &a, "foo=bar")
+	testArgsString(t, &a, "foo=bar&baz=sss")
+	testArgsString(t, &a, "")
+	testArgsString(t, &a, "foo=bar&aa=bbb&%E8%96%9B=%E8%9B%9F&=xxxx&cvx")
+	testArgsString(t, &a, "=asdfsdf")
 }
 
 func testArgsString(t *testing.T, a *Args, s string) {
-    a.Parse(s)
-    s1 := a.String()
-    if s != s1 {
-        t.Fatalf("Unexpected args %q. Expected %q", s1, s)
-    }
+	a.Parse(s)
+	s1 := a.String()
+	if s != s1 {
+		t.Fatalf("Unexpected args %q. Expected %q", s1, s)
+	}
 }
 
 func TestArgsParse(t *testing.T) {
-    var a Args
+	var a Args
 
-    // empty args
-    testArgsParse(t, &a, "", 0, "foo=", "bar=", "=")
+	// empty args
+	testArgsParse(t, &a, "", 0, "foo=", "bar=", "=")
 
-    // arg without value
-    testArgsParse(t, &a, "foo1", 1, "foo=", "bar=", "=")
+	// arg without value
+	testArgsParse(t, &a, "foo1", 1, "foo=", "bar=", "=")
 
-    // arg without value, but with equal sign
-    testArgsParse(t, &a, "foo2=", 1, "foo=", "bar=", "=")
+	// arg without value, but with equal sign
+	testArgsParse(t, &a, "foo2=", 1, "foo=", "bar=", "=")
 
-    // arg with value
-    testArgsParse(t, &a, "foo3=bar1", 1, "foo3=bar1", "bar=", "=")
+	// arg with value
+	testArgsParse(t, &a, "foo3=bar1", 1, "foo3=bar1", "bar=", "=")
 
-    // empty key
-    testArgsParse(t, &a, "=bar2", 1, "foo=", "=bar2", "bar2=")
+	// empty key
+	testArgsParse(t, &a, "=bar2", 1, "foo=", "=bar2", "bar2=")
 
-    // missing kv
-    testArgsParse(t, &a, "&&&&", 0, "foo=", "bar=", "=")
+	// missing kv
+	testArgsParse(t, &a, "&&&&", 0, "foo=", "bar=", "=")
 
-    // multiple value with the same key
-    testArgsParse(t, &a, "x=1&x=2&x=3", 3, "x=1")
+	// multiple value with the same key
+	testArgsParse(t, &a, "x=1&x=2&x=3", 3, "x=1")
 
-    // multiple args without values
-    testArgsParse(t, &a, "&&a&&b&&bar&baz", 4, "a=", "b=", "bar=", "baz=")
+	// multiple args without values
+	testArgsParse(t, &a, "&&a&&b&&bar&baz", 4, "a=", "b=", "bar=", "baz=")
 
-    // values with '='
-    testArgsParse(t, &a, "zz=1&k=v=v=a=a=s", 2, "k=v=v=a=a=s", "zz=1")
+	// values with '='
+	testArgsParse(t, &a, "zz=1&k=v=v=a=a=s", 2, "k=v=v=a=a=s", "zz=1")
 
-    // mixed '=' and '&'
-    testArgsParse(t, &a, "sss&z=dsf=&df", 3, "sss=", "z=dsf=", "df=")
+	// mixed '=' and '&'
+	testArgsParse(t, &a, "sss&z=dsf=&df", 3, "sss=", "z=dsf=", "df=")
 
-    // encoded args
-    testArgsParse(t, &a, "%E8%96%9B=%E8%9B%9F", 1, "薛=蛟")
+	// encoded args
+	testArgsParse(t, &a, "%E8%96%9B=%E8%9B%9F", 1, "薛=蛟")
 
-    // invalid percent encoding
-    testArgsParse(t, &a, "f%=x&qw%z=d%0k%20p&%%20=%%%20x", 3, "f%=x", "qw%z=d%0k p", "% =%% x")
+	// invalid percent encoding
+	testArgsParse(t, &a, "f%=x&qw%z=d%0k%20p&%%20=%%%20x", 3, "f%=x", "qw%z=d%0k p", "% =%% x")
 }
 
 func testArgsParse(t *testing.T, a *Args, s string, expectedLen int, expectedArgs ...string) {
-    var buf []byte
-    a.Parse(s)
-    if a.Len() != expectedLen {
-        t.Fatalf("Unexpected args len %d. Expected %d. s=%q", a.Len(), expectedLen, s)
-    }
-    for _, xx := range expectedArgs {
-        tmp := strings.SplitN(xx, "=", 2)
-        k := tmp[0]
-        v := tmp[1]
-        buf = a.GetBytes(buf, k)
-        if string(buf) != v {
-            t.Fatalf("Unexpected value for key=%q: %q. Expected %q. s=%q", k, buf, v, s)
-        }
-    }
+	var buf []byte
+	a.Parse(s)
+	if a.Len() != expectedLen {
+		t.Fatalf("Unexpected args len %d. Expected %d. s=%q", a.Len(), expectedLen, s)
+	}
+	for _, xx := range expectedArgs {
+		tmp := strings.SplitN(xx, "=", 2)
+		k := tmp[0]
+		v := tmp[1]
+		buf = a.GetBytes(buf, k)
+		if string(buf) != v {
+			t.Fatalf("Unexpected value for key=%q: %q. Expected %q. s=%q", k, buf, v, s)
+		}
+	}
 }
 
 func TestArgsHas(t *testing.T) {
-    var a Args
+	var a Args
 
-    // single arg
-    testArgsHas(t, &a, "foo", "foo")
-    testArgsHasNot(t, &a, "foo", "bar", "baz", "")
+	// single arg
+	testArgsHas(t, &a, "foo", "foo")
+	testArgsHasNot(t, &a, "foo", "bar", "baz", "")
 
-    // multi args without values
-    testArgsHas(t, &a, "foo&bar", "foo", "bar")
-    testArgsHasNot(t, &a, "foo&bar", "", "aaaa")
+	// multi args without values
+	testArgsHas(t, &a, "foo&bar", "foo", "bar")
+	testArgsHasNot(t, &a, "foo&bar", "", "aaaa")
 
-    // multi args
-    testArgsHas(t, &a, "b=xx&=aaa&c=", "", "c", "b")
-    testArgsHasNot(t, &a, "b=xx&=aaa&c=", "xx", "aaa", "foo")
+	// multi args
+	testArgsHas(t, &a, "b=xx&=aaa&c=", "", "c", "b")
+	testArgsHasNot(t, &a, "b=xx&=aaa&c=", "xx", "aaa", "foo")
 
-    // encoded args
-    testArgsHas(t, &a, "a+b=c+d%20%20e", "a b")
-    testArgsHasNot(t, &a, "a+b=c+d", "a+b", "c+d")
+	// encoded args
+	testArgsHas(t, &a, "a+b=c+d%20%20e", "a b")
+	testArgsHasNot(t, &a, "a+b=c+d", "a+b", "c+d")
 }
 
 func testArgsHas(t *testing.T, a *Args, s string, expectedKeys ...string) {
-    a.Parse(s)
-    for _, key := range expectedKeys {
-        if !a.Has(key) {
-            t.Fatalf("Missing key %q in %q", key, s)
-        }
-    }
+	a.Parse(s)
+	for _, key := range expectedKeys {
+		if !a.Has(key) {
+			t.Fatalf("Missing key %q in %q", key, s)
+		}
+	}
 }
 
 func testArgsHasNot(t *testing.T, a *Args, s string, unexpectedKeys ...string) {
-    a.Parse(s)
-    for _, key := range unexpectedKeys {
-        if a.Has(key) {
-            t.Fatalf("Unexpected key %q in %q", key, s)
-        }
-    }
+	a.Parse(s)
+	for _, key := range unexpectedKeys {
+		if a.Has(key) {
+			t.Fatalf("Unexpected key %q in %q", key, s)
+		}
+	}
+}
+
+func TestArgsSetGetDel(t *testing.T) {
+	var a Args
+
+	if a.Get("foo") != "" {
+		t.Fatalf("Unexpected value: %q", a.Get("foo"))
+	}
+	if a.Get("") != "" {
+		t.Fatalf("Unexpected value: %q", a.Get(""))
+	}
+	a.Del("xxx")
+
+	for j := 0; j < 3; j++ {
+		for i := 0; i < 10; i++ {
+			k := fmt.Sprintf("foo%d", i)
+			v := fmt.Sprintf("bar_%d", i)
+			a.Set(k, v)
+			if a.Get(k) != v {
+				t.Fatalf("Unexpected value: %q. Expected %q", a.Get(k), v)
+			}
+		}
+	}
+	for i := 0; i < 10; i++ {
+		k := fmt.Sprintf("foo%d", i)
+		v := fmt.Sprintf("bar_%d", i)
+		if a.Get(k) != v {
+			t.Fatalf("Unexpected value: %q. Expected %q", a.Get(k), v)
+		}
+		a.Del(k)
+		if a.Get(k) != "" {
+			t.Fatalf("Unexpected value: %q. Expected %q", a.Get(k), "")
+		}
+	}
+
+	a.Parse("aaa=xxx&bb=aa")
+	if a.Get("foo0") != "" {
+		t.Fatalf("Unexpected value %q", a.Get("foo0"))
+	}
+	if a.Get("aaa") != "xxx" {
+		t.Fatalf("Unexpected value %q. Expected %q", a.Get("aaa"), "xxx")
+	}
+	if a.Get("bb") != "aa" {
+		t.Fatalf("Unexpected value %q. Expected %q", a.Get("bb"), "aa")
+	}
+	for i := 0; i < 10; i++ {
+		k := fmt.Sprintf("xx%d", i)
+		v := fmt.Sprintf("yy%d", i)
+		a.Set(k, v)
+		if a.Get(k) != v {
+			t.Fatalf("Unexpected value %q. Expected %q", a.Get(k), v)
+		}
+	}
+	for i := 5; i < 10; i++ {
+		k := fmt.Sprintf("xx%d", i)
+		v := fmt.Sprintf("yy%d", i)
+		if a.Get(k) != v {
+			t.Fatalf("Unexpected value %q. Expected %q", a.Get(k), v)
+		}
+		a.Del(k)
+		if a.Get(k) != "" {
+			t.Fatalf("Unexpected value %q. Expected %q", a.Get(k), "")
+		}
+	}
 }
