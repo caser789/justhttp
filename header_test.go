a163913a5e98e421080ca0a4f38caa818f6191de
Xue Jiao <jiao.xue@shopee.com>
2021-07-06 11:51:30 +0800

    TestResponseHeaderReadSuccess

--- header_test.go
+++ header_test.go
@@ -51,14 +51,6 @@ func TestResponseHeaderTooBig(t *testing.T) {
 	}
 }
 
-func getHeaders(n int) string {
-	var h []string
-	for i := 0; i < n; i++ {
-		h = append(h, fmt.Sprintf("Header_%d: Value_%d\r\n", i, i))
-	}
-	return strings.Join(h, "")
-}
-
 func TestResponseHeaderBufioPeek(t *testing.T) {
 	r := &bufioPeekReader{
 		s: "HTTP/1.1 200 OK\r\nContent-Length: 10\r\nContent-Type: aaa\r\n" + getHeaders(10) + "\r\n0123456789",
@@ -70,6 +62,101 @@ func TestResponseHeaderBufioPeek(t *testing.T) {
 	}
 	verifyResponseHeader(t, h, 200, 10, "aaa")
 	verifyTrailer(t, br, "0123456789")
+}
+
+func TestResponseHeaderReadSuccess(t *testing.T) {
+	h := &ResponseHeader{}
+
+	// straight order of content-length and content-type
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Length: 123\r\nContent-Type: text/html\r\n\r\n",
+		200, 123, "text/html", "")
+
+	// reverse order of content-length and content-type
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 202 OK\r\nContent-Type: text/plain; encoding=utf-8\r\nContent-length: 543\r\n\r\n",
+		202, 543, "text/plain; encoding=utf-8", "")
+
+	// transfer-encoding: chunked
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 505 Internal error\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\n\r\n",
+		505, -1, "text/html", "")
+
+	// reverse order of content-type and transfer-encoding
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 343 foobar\r\nTransfer-Encoding: chunked\r\nContent-Type: text/json\r\n\r\n",
+		343, -1, "text/json", "")
+
+	// additional headers
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 100 Continue\r\nFooBar: baz\r\nContent-Type: aaa/bbb\r\nUser-Agent: x\r\nContent-Length: 123\r\nZZZ: werer\r\n\r\n",
+		100, 123, "aaa/bbb", "")
+
+	// trailer (aka body)
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 32245\r\n\r\nqwert aaa",
+		200, 32245, "text/plain", "qwert aaa")
+
+	// ancient http protocol
+	testResponseHeaderReadSuccess(t, h, "HTTP/0.9 300 OK\r\nContent-Length: 123\r\nContent-Type: text/html\r\n\r\nqqqq",
+		300, 123, "text/html", "qqqq")
+
+	// lf instead of crlf
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\nContent-Length: 123\nContent-Type: text/html\n\n",
+		200, 123, "text/html", "")
+
+	// Zero-length headers with mixed crlf and lf
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 400 OK\nContent-Length: 345\nZero-Value: \r\nContent-Type: aaa\n: zero-key\r\n\r\nooa",
+		400, 345, "aaa", "ooa")
+
+	// No space after colon
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\nContent-Length:34\nContent-Type: sss\n\naaaa",
+		200, 34, "sss", "aaaa")
+
+	// invalid case
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 400 OK\nconTEnt-leNGTH: 123\nConTENT-TYPE: ass\n\n",
+		400, 123, "ass", "")
+
+	// duplicate content-length
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Length: 456\r\nContent-Type: foo/bar\r\nContent-Length: 321\r\n\r\n",
+		200, 321, "foo/bar", "")
+
+	// duplicate content-type
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Length: 234\r\nContent-Type: foo/bar\r\nContent-Type: baz/bar\r\n\r\n",
+		200, 234, "baz/bar", "")
+
+	// both transfer-encoding: chunked and content-length
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Type: foo/bar\r\nContent-Length: 123\r\nTransfer-Encoding: chunked\r\n\r\n",
+		200, -1, "foo/bar", "")
+
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 300 OK\r\nContent-Type: foo/barr\r\nTransfer-Encoding: chunked\r\nContent-Length: 354\r\n\r\n",
+		300, -1, "foo/barr", "")
+
+	// duplicate transfer-encoding: chunked
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nTransfer-Encoding: chunked\r\nTransfer-Encoding: chunked\r\n\r\n",
+		200, -1, "text/html", "")
+
+	// no reason string in the first line
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 456 OK\r\nContent-Type: xxx/yyy\r\nContent-Length: 134\r\n\r\naaaxxx",
+		456, 134, "xxx/yyy", "aaaxxx")
+
+	// blank lines before the first line
+	testResponseHeaderReadSuccess(t, h, "\r\nHTTP/1.1 200 OK\r\nContent-Type: aa\r\nContent-Length: 0\r\n\r\nsss",
+		200, 0, "aa", "sss")
+}
+
+func testResponseHeaderReadSuccess(t *testing.T, h *ResponseHeader, headers string, expectedStatusCode, expectedContentLength int,
+	expectedContentType, expectedTrailer string) {
+	r := bytes.NewBufferString(headers)
+	br := bufio.NewReader(r)
+	err := h.Read(br)
+	if err != nil {
+		t.Fatalf("Unexpected error when parsing response headers: %s. headers=%q", err, headers)
+	}
+	verifyResponseHeader(t, h, expectedStatusCode, expectedContentLength, expectedContentType)
+	verifyTrailer(t, br, expectedTrailer)
+}
+
+func getHeaders(n int) string {
+	var h []string
+	for i := 0; i < n; i++ {
+		h = append(h, fmt.Sprintf("Header_%d: Value_%d\r\n", i, i))
+	}
+	return strings.Join(h, "")
 }
 
 type bufioPeekReader struct {
