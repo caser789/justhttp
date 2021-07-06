046233bc7bb3a881add9f053bfa52e5a8b41dda0
Xue Jiao <jiao.xue@shopee.com>
2021-07-06 21:15:13 +0800

    TestRequestReadChunked

--- http_test.go
+++ http_test.go
@@ -3,6 +3,7 @@ package fasthttp
 import (
 	"bufio"
 	"bytes"
+	"strings"
 	"testing"
 )
 
@@ -96,4 +97,107 @@ func testRequestWriteError(t *testing.T, method, requestURI, host, userAgent, bo
 	if err == nil {
 		t.Fatalf("Expecting error when writing request=%#v", req)
 	}
+}
+
+func TestRequestParseURI(t *testing.T) {
+	host := "foobar.com"
+	requestURI := "/aaa/bb+b%20d?ccc=ddd&qqq#1334dfds&=d"
+	expectedPathOriginal := "/aaa/bb+b%20d"
+	expectedPath := "/aaa/bb+b d"
+	expectedQueryString := "ccc=ddd&qqq"
+	expectedHash := "1334dfds&=d"
+
+	var req Request
+	req.Header.Host = []byte(host)
+	req.Header.RequestURI = []byte(requestURI)
+
+	req.ParseURI()
+
+	if string(req.URI.Host) != host {
+		t.Fatalf("Unexpected host %q. Expected %q", req.URI.Host, host)
+	}
+	if string(req.URI.PathOriginal) != expectedPathOriginal {
+		t.Fatalf("Unexpected source path %q. Expected %q", req.URI.PathOriginal, expectedPathOriginal)
+	}
+	if string(req.URI.Path) != expectedPath {
+		t.Fatalf("Unexpected path %q. Expected %q", req.URI.Path, expectedPath)
+	}
+	if string(req.URI.QueryString) != expectedQueryString {
+		t.Fatalf("Unexpected query string %q. Expected %q", req.URI.QueryString, expectedQueryString)
+	}
+	if string(req.URI.Hash) != expectedHash {
+		t.Fatalf("Unexpected hash %q. Expected %q", req.URI.Hash, expectedHash)
+	}
+}
+
+func TestRequestParsePostArgsSuccess(t *testing.T) {
+	var req Request
+
+	testRequestParsePostArgsSuccess(t, &req, "POST / HTTP/1.1\r\nHost: aaa.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 0\r\n\r\n", 0, "foo=", "=")
+
+	testRequestParsePostArgsSuccess(t, &req, "POST / HTTP/1.1\r\nHost: aaa.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 18\r\n\r\nfoo&b%20r=b+z=&qwe", 3, "foo=", "b r=b z=", "qwe=")
+}
+
+func testRequestParsePostArgsSuccess(t *testing.T, req *Request, s string, expectedArgsLen int, expectedArgs ...string) {
+	r := bytes.NewBufferString(s)
+	br := bufio.NewReader(r)
+	err := req.Read(br)
+	if err != nil {
+		t.Fatalf("Unexpected error when reading %q: %s", s, err)
+	}
+	if err = req.ParsePostArgs(); err != nil {
+		t.Fatalf("Unexpected error when parsing POST args for %q: %s", s, err)
+	}
+	if req.PostArgs.Len() != expectedArgsLen {
+		t.Fatalf("Unexpected args len %d. Expected %d for %q", req.PostArgs.Len(), expectedArgsLen, s)
+	}
+	for _, x := range expectedArgs {
+		tmp := strings.SplitN(x, "=", 2)
+		k := tmp[0]
+		v := tmp[1]
+		vv := req.PostArgs.Get(k)
+		if vv != v {
+			t.Fatalf("Unexpected value for key %q: %q. Expected %q for %q", k, vv, v, s)
+		}
+	}
+}
+
+func TestRequestParsePostArgsError(t *testing.T) {
+	var req Request
+
+	// non-post
+	testRequestParsePostArgsError(t, &req, "GET /aa HTTP/1.1\r\nHost: aaa\r\n\r\n")
+
+	// invalid content-type
+	testRequestParsePostArgsError(t, &req, "POST /aa HTTP/1.1\r\nHost: aaa\r\nContent-Type: text/html\r\nContent-Length: 5\r\n\r\nabcde")
+}
+
+func testRequestParsePostArgsError(t *testing.T, req *Request, s string) {
+	r := bytes.NewBufferString(s)
+	br := bufio.NewReader(r)
+	err := req.Read(br)
+	if err != nil {
+		t.Fatalf("Unexpected error when reading %q: %s", s, err)
+	}
+	if err = req.ParsePostArgs(); err == nil {
+		t.Fatalf("Expecting error when parsing POST args for %q", s)
+	}
+}
+
+func TestRequestReadChunked(t *testing.T) {
+	var req Request
+
+	s := "POST /foo HTTP/1.1\r\nHost: google.com\r\nTransfer-Encoding: chunked\r\nContent-Type: aa/bb\r\n\r\n3\r\nabc\r\n5\r\n12345\r\n0\r\n\r\ntrail"
+	r := bytes.NewBufferString(s)
+	rb := bufio.NewReader(r)
+	err := req.Read(rb)
+	if err != nil {
+		t.Fatalf("Unexpected error when reading chunked request: %s", err)
+	}
+	expectedBody := "abc12345"
+	if string(req.Body) != expectedBody {
+		t.Fatalf("Unexpected body %q. Expected %q", req.Body, expectedBody)
+	}
+	verifyRequestHeader(t, &req.Header, -1, "/foo", "google.com", "", "aa/bb")
+	verifyTrailer(t, rb, "trail")
 }
