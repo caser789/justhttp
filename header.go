a15f74385fc2106f1ad9dc32064335785cd3d7ff
Xue Jiao <jiao.xue@seamoney.com>
2021-07-12 09:29:48 +0800

    Support identity response

--- header.go
+++ header.go
@@ -40,7 +40,9 @@ type ResponseHeader struct {
 
 	// Resposne content length read from Content-Length header.
 	//
-	// It may be negative on chunked response.
+	// It may be negative:
+	// -1 means Transfer-Encoding: chunked.
+	// -2 means Transfer-Encoding: identity.
 	ContentLength int
 
 	contentType []byte
@@ -297,6 +299,7 @@ func (h *ResponseHeader) parseFirstLine(buf []byte) (b []byte, err error) {
 }
 
 func (h *ResponseHeader) parseHeaders(buf []byte) ([]byte, error) {
+	// 'identity' content-length by default
 	h.ContentLength = -2
 
 	var s headerScanner
@@ -320,7 +323,7 @@ func (h *ResponseHeader) parseHeaders(buf []byte) ([]byte, error) {
 				}
 			}
 		case bytes.Equal(s.key, strTransferEncoding):
-			if bytes.Equal(s.value, strChunked) {
+			if !bytes.Equal(s.value, strIdentity) {
 				h.ContentLength = -1
 			}
 		case bytes.Equal(s.key, strConnection):
@@ -345,7 +348,8 @@ func (h *ResponseHeader) parseHeaders(buf []byte) ([]byte, error) {
 		return nil, fmt.Errorf("missing required Content-Type header in %q", buf)
 	}
 	if h.ContentLength == -2 {
-		return nil, fmt.Errorf("missing both Content-Length and Transfer-Encoding: chunked in %q", buf)
+		// Close connection after 'identity' response.
+		h.ConnectionClose = true
 	}
 	return s.b, nil
 }
@@ -507,7 +511,8 @@ type RequestHeader struct {
 
 	// Request content length read from Content-Length header.
 	//
-	// It may be negative on chunked request.
+	// It may be negative:
+	// -1 means Transfer-Encoding: chunked.
 	ContentLength int
 
 	// Set to true if request contains 'Connection: close' header.
@@ -865,7 +870,7 @@ func (h *RequestHeader) parseHeaders(buf []byte) ([]byte, error) {
 				}
 			}
 		case bytes.Equal(s.key, strTransferEncoding):
-			if bytes.Equal(s.value, strChunked) {
+			if !bytes.Equal(s.value, strIdentity) {
 				h.ContentLength = -1
 			}
 		case bytes.Equal(s.key, strConnection):
--- header_test.go
+++ header_test.go
@@ -622,6 +622,16 @@ func TestResponseHeaderReadSuccess(t *testing.T) {
 	// blank lines before the first line
 	testResponseHeaderReadSuccess(t, h, "\r\nHTTP/1.1 200 OK\r\nContent-Type: aa\r\nContent-Length: 0\r\n\r\nsss",
 		200, 0, "aa", "sss")
+	if h.ConnectionClose {
+		t.Fatalf("unexpected connection: close")
+	}
+
+	// no content-length (identity transfer-encoding)
+	testResponseHeaderReadSuccess(t, h, "HTTP/1.1 200 OK\r\nContent-Type: foo/bar\r\n\r\nabcdefg",
+		200, -2, "foo/bar", "abcdefg")
+	if !h.ConnectionClose {
+		t.Fatalf("unexpected connection: close for identity response")
+	}
 }
 
 func TestResponseHeaderReadError(t *testing.T) {
@@ -653,9 +663,6 @@ func TestResponseHeaderReadError(t *testing.T) {
 
 	// no content-type
 	testResponseHeaderReadError(t, h, "HTTP/1.1 200 OK\r\nContent-Length: 123\r\n\r\n")
-
-	// no content-length
-	testResponseHeaderReadError(t, h, "HTTP/1.1 200 OK\r\nContent-Type: foo/bar\r\n\r\n")
 }
 
 func TestRequestHeaderTooBig(t *testing.T) {
