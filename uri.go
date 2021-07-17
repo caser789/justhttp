d3a045dc5289a51ce486e1ca1322c57a7ffcc033
Xue Jiao <jiao.xuejiao@gmail.com>
2021-07-17 19:28:38 +0800

    Fix fragment parsing so it won't get url encoded if there is no query in front of it

--- uri.go
+++ uri.go
@@ -7,7 +7,7 @@ import (
 
 // URI represents URI :) .
 //
-// It is forbidden copying URI instances. Create new instances and use CopyTo
+// It is forbidden copying URI instances. Create new instance and use CopyTo
 // instead.
 type URI struct {
 	pathOriginal []byte
@@ -26,15 +26,6 @@ type URI struct {
 	h *RequestHeader
 }
 
-func (x *URI) LastPathSegment() []byte {
-	path := x.Path()
-	n := bytes.LastIndexByte(path, '/')
-	if n < 0 {
-		return path
-	}
-	return path[n+1:]
-}
-
 // CopyTo copies uri contents to dst.
 func (x *URI) CopyTo(dst *URI) {
 	dst.Reset()
@@ -48,11 +39,11 @@ func (x *URI) CopyTo(dst *URI) {
 	dst.parsedQueryArgs = false
 
 	// fullURI and requestURI shouldn't be copied, since they are created
-	// from scratch on each FULLURI() and RequestURI() call.
+	// from scratch on each FullURI() and RequestURI() call.
 	dst.h = x.h
 }
 
-// Hash returns URI hash, i.e. qwe of http://aaa.com/foo/bar?baz=123#qwe.
+// Hash returns URI hash, i.e. qwe of http://aaa.com/foo/bar?baz=123#qwe .
 //
 // The returned value is valid until the next URI method call.
 func (x *URI) Hash() []byte {
@@ -70,7 +61,7 @@ func (x *URI) SetHashBytes(hash []byte) {
 }
 
 // QueryString returns URI query string,
-// i.e. baz=123 of http://aaa.com/foo/bar?baz=123#qwe.
+// i.e. baz=123 of http://aaa.com/foo/bar?baz=123#qwe .
 //
 // The returned value is valid until the next URI method call.
 func (x *URI) QueryString() []byte {
@@ -124,7 +115,7 @@ func (x *URI) PathOriginal() []byte {
 
 // Scheme returns URI scheme, i.e. http of http://aaa.com/foo/bar?baz=123#qwe .
 //
-// returned scheme is always lowercased.
+// Returned scheme is always lowercased.
 //
 // The returned value is valid until the next URI method call.
 func (x *URI) Scheme() []byte {
@@ -141,7 +132,7 @@ func (x *URI) SetScheme(scheme string) {
 	lowercaseBytes(x.scheme)
 }
 
-// SetScheme sets URI scheme, i.e. http, https, ftp, etc.
+// SetSchemeBytes sets URI scheme, i.e. http, https, ftp, etc.
 func (x *URI) SetSchemeBytes(scheme []byte) {
 	x.scheme = append(x.scheme[:0], scheme...)
 	lowercaseBytes(x.scheme)
@@ -155,18 +146,29 @@ func (x *URI) Reset() {
 	x.queryString = x.queryString[:0]
 	x.hash = x.hash[:0]
 
+	x.host = x.host[:0]
 	x.queryArgs.Reset()
 	x.parsedQueryArgs = false
 
-	x.host = x.host[:0]
-
 	// There is no need in x.fullURI = x.fullURI[:0], since full uri
-	// is calculated on each call to FULLURI().
+	// is calucalted on each call to FullURI().
 
 	// There is no need in x.requestURI = x.requestURI[:0], since requestURI
 	// is calculated on each call to RequestURI().
 
 	x.h = nil
+}
+
+// Host returns host part, i.e. aaa.com of http://aaa.com/foo/bar?baz=123#qwe .
+//
+// Host is always lowercased.
+func (x *URI) Host() []byte {
+	if len(x.host) == 0 && x.h != nil {
+		x.host = append(x.host[:0], x.h.Host()...)
+		lowercaseBytes(x.host)
+		x.h = nil
+	}
+	return x.host
 }
 
 // SetHost sets host for the uri.
@@ -181,25 +183,172 @@ func (x *URI) SetHostBytes(host []byte) {
 	lowercaseBytes(x.host)
 }
 
-// Host returns host part
-//
-// Host is always lowercased.
-func (x *URI) Host() []byte {
-	if len(x.host) == 0 && x.h != nil {
-		x.host = append(x.host[:0], x.h.Host()...)
-		lowercaseBytes(x.host)
-		x.h = nil
+// Parse initializes URI from the given host and uri.
+func (x *URI) Parse(host, uri []byte) {
+	x.parse(host, uri, nil)
+}
+
+func (x *URI) parseQuick(uri []byte, h *RequestHeader) {
+	x.parse(nil, uri, h)
+}
+
+func (x *URI) parse(host, uri []byte, h *RequestHeader) {
+	x.Reset()
+	x.h = h
+
+	scheme, host, uri := splitHostURI(host, uri)
+	x.scheme = append(x.scheme, scheme...)
+	lowercaseBytes(x.scheme)
+	x.host = append(x.host, host...)
+	lowercaseBytes(x.host)
+
+	b := uri
+	queryIndex := bytes.IndexByte(b, '?')
+	fragmentIndex := bytes.IndexByte(b, '#')
+	// Ignore query in fragment part
+	if fragmentIndex >= 0 && queryIndex > fragmentIndex {
+		queryIndex = -1
 	}
-	return x.host
+
+	if queryIndex < 0 && fragmentIndex < 0 {
+		x.pathOriginal = append(x.pathOriginal, b...)
+		x.path = normalizePath(x.path, b)
+		return
+	}
+
+	if queryIndex >= 0 {
+		// Path is everything up to the start of the query
+		x.pathOriginal = append(x.pathOriginal, b[:queryIndex]...)
+		x.path = normalizePath(x.path, x.pathOriginal)
+
+		if fragmentIndex < 0 {
+			x.queryString = append(x.queryString, b[queryIndex+1:]...)
+		} else {
+			x.queryString = append(x.queryString, b[queryIndex+1:fragmentIndex]...)
+		}
+	}
+
+	if fragmentIndex >= 0 {
+		if queryIndex < 0 {
+			// Path is up to the start of fragment. Unless a query was before it
+			x.pathOriginal = append(x.pathOriginal, b[:fragmentIndex]...)
+			x.path = normalizePath(x.path, x.pathOriginal)
+		}
+
+		x.hash = append(x.hash, b[fragmentIndex+1:]...)
+		b = b[:fragmentIndex]
+	}
+}
+
+func normalizePath(dst, src []byte) []byte {
+	dst = dst[:0]
+
+	// add leading slash
+	if len(src) == 0 || src[0] != '/' {
+		dst = append(dst, '/')
+	}
+
+	dst = decodeArgAppend(dst, src, false)
+
+	// remove duplicate slashes
+	b := dst
+	bSize := len(b)
+	for {
+		n := bytes.Index(b, strSlashSlash)
+		if n < 0 {
+			break
+		}
+		b = b[n:]
+		copy(b, b[1:])
+		b = b[:len(b)-1]
+		bSize--
+	}
+	dst = dst[:bSize]
+
+	// remove /foo/../ parts
+	b = dst
+	for {
+		n := bytes.Index(b, strSlashDotDotSlash)
+		if n < 0 {
+			break
+		}
+		nn := bytes.LastIndexByte(b[:n], '/')
+		if nn < 0 {
+			nn = 0
+		}
+		n += len(strSlashDotDotSlash) - 1
+		copy(b[nn:], b[n:])
+		b = b[:len(b)-n+nn]
+	}
+
+	// remove /./ parts
+	for {
+		n := bytes.Index(b, strSlashDotSlash)
+		if n < 0 {
+			break
+		}
+		nn := n + len(strSlashDotSlash) - 1
+		copy(b[n:], b[nn:])
+		b = b[:len(b)-nn+n]
+	}
+
+	// remove trailing /foo/..
+	n := bytes.LastIndex(b, strSlashDotDot)
+	if n >= 0 && n+len(strSlashDotDot) == len(b) {
+		nn := bytes.LastIndexByte(b[:n], '/')
+		if nn < 0 {
+			return strSlash
+		}
+		b = b[:nn+1]
+	}
+
+	return b
+}
+
+// RequestURI returns RequestURI - i.e. URI without Scheme and Host.
+func (x *URI) RequestURI() []byte {
+	dst := appendQuotedPath(x.requestURI[:0], x.Path())
+	if x.queryArgs.Len() > 0 {
+		dst = append(dst, '?')
+		dst = x.queryArgs.AppendBytes(dst)
+	} else if len(x.queryString) > 0 {
+		dst = append(dst, '?')
+		dst = append(dst, x.queryString...)
+	}
+	if len(x.hash) > 0 {
+		dst = append(dst, '#')
+		dst = append(dst, x.hash...)
+	}
+	x.requestURI = dst
+	return x.requestURI
+}
+
+// LastPathSegment returns the last part of uri path after '/'.
+//
+// Examples:
+//
+//    * For /foo/bar/baz.html path returns baz.html.
+//    * For /foo/bar/ returns empty byte slice.
+//    * For /foobar.js returns foobar.js.
+func (x *URI) LastPathSegment() []byte {
+	path := x.Path()
+	n := bytes.LastIndexByte(path, '/')
+	if n < 0 {
+		return path
+	}
+	return path[n+1:]
 }
 
 // Update updates uri.
 //
 // The following newURI types are accepted:
 //
-//    * Absoluet
-//    * Missing host
-//    * Relative path
+//     * Absolute, i.e. http://foobar.com/aaa/bb?cc . In this case the original
+//       uri is replaced by newURI.
+//     * Missing host, i.e. /aaa/bb?cc . In this case only RequestURI part
+//       of the original uri is replaced.
+//     * Relative path, i.e.  xx?yy=abc . In this case the original RequestURI
+//       is updated according to the new relative path.
 func (x *URI) Update(newURI string) {
 	x.fullURI = append(x.fullURI[:0], newURI...)
 	x.UpdateBytes(x.fullURI)
@@ -209,9 +358,12 @@ func (x *URI) Update(newURI string) {
 //
 // The following newURI types are accepted:
 //
-//    * Absoluet
-//    * Missing host
-//    * Relative path
+//     * Absolute, i.e. http://foobar.com/aaa/bb?cc . In this case the original
+//       uri is replaced by newURI.
+//     * Missing host, i.e. /aaa/bb?cc . In this case only RequestURI part
+//       of the original uri is replaced.
+//     * Relative path, i.e.  xx?yy=abc . In this case the original RequestURI
+//       is updated according to the new relative path.
 func (x *URI) UpdateBytes(newURI []byte) {
 	x.requestURI = x.updateBytes(newURI, x.requestURI)
 }
@@ -254,53 +406,13 @@ func (x *URI) updateBytes(newURI, buf []byte) []byte {
 	return buf
 }
 
-// Parse initializes URI from the given host and uri.
-func (x *URI) Parse(host, uri []byte) {
-	x.parse(host, uri, nil)
-}
-
-func (x *URI) parseQuick(uri []byte, h *RequestHeader) {
-	x.parse(nil, uri, h)
-}
-
-func (x *URI) parse(host, uri []byte, h *RequestHeader) {
-	x.Reset()
-	x.h = h
-
-	scheme, host, uri := splitHostUri(host, uri)
-	x.scheme = append(x.scheme, scheme...)
-	lowercaseBytes(x.scheme)
-	x.host = append(x.host, host...)
-	lowercaseBytes(x.host)
-
-	b := uri
-	n := bytes.IndexByte(b, '?')
-	if n < 0 {
-		x.pathOriginal = append(x.pathOriginal, b...)
-		x.path = normalizePath(x.path, b)
-		return
-	}
-	x.pathOriginal = append(x.pathOriginal, b[:n]...)
-	x.path = normalizePath(x.path, x.pathOriginal)
-	b = b[n+1:]
-
-	n = bytes.IndexByte(b, '#')
-	if n >= 0 {
-		x.hash = append(x.hash, b[n+1:]...)
-		b = b[:n]
-	}
-
-	x.queryString = append(x.queryString, b...)
-}
-
 // FullURI returns full uri in the form {Scheme}://{Host}{RequestURI}#{Hash}.
 func (x *URI) FullURI() []byte {
 	x.fullURI = x.AppendBytes(x.fullURI[:0])
 	return x.fullURI
 }
 
-// AppendBytes appends full uri to dst and returns dst
-// (which may be newly allocated).
+// AppendBytes appends full uri to dst and returns the extended dst.
 func (x *URI) AppendBytes(dst []byte) []byte {
 	dst = x.appendSchemeHost(dst)
 	return append(dst, x.RequestURI()...)
@@ -314,7 +426,7 @@ func (x *URI) appendSchemeHost(dst []byte) []byte {
 
 // WriteTo writes full uri to w.
 //
-// WriteTo implements io.WriteTo interface.
+// WriteTo implements io.WriterTo interface.
 func (x *URI) WriteTo(w io.Writer) (int64, error) {
 	n, err := w.Write(x.FullURI())
 	return int64(n), err
@@ -325,25 +437,7 @@ func (x *URI) String() string {
 	return string(x.FullURI())
 }
 
-// RequestURI returns RequestURI - i.e. URI without Scheme and Host.
-func (x *URI) RequestURI() []byte {
-	dst := appendQuotedPath(x.requestURI[:0], x.Path())
-	if x.queryArgs.Len() > 0 {
-		dst = append(dst, '?')
-		dst = x.queryArgs.AppendBytes(dst)
-	} else if len(x.queryString) > 0 {
-		dst = append(dst, '?')
-		dst = append(dst, x.queryString...)
-	}
-	if len(x.hash) > 0 {
-		dst = append(dst, '#')
-		dst = append(dst, x.hash...)
-	}
-	x.requestURI = dst
-	return x.requestURI
-}
-
-func splitHostUri(host, uri []byte) ([]byte, []byte, []byte) {
+func splitHostURI(host, uri []byte) ([]byte, []byte, []byte) {
 	n := bytes.Index(uri, strColonSlashSlash)
 	if n < 0 {
 		return strHTTP, host, uri
@@ -361,71 +455,7 @@ func splitHostUri(host, uri []byte) ([]byte, []byte, []byte) {
 	return scheme, uri[:n], uri[n:]
 }
 
-func normalizePath(dst, src []byte) []byte {
-	dst = dst[:0]
-
-	// add leading slash
-	if len(src) == 0 || src[0] != '/' {
-		dst = append(dst, '/')
-	}
-	dst = decodeArgAppend(dst, src, false)
-
-	// remove duplicate slashes
-	b := dst
-	bSize := len(b)
-	for {
-		n := bytes.Index(b, strSlashSlash)
-		if n < 0 {
-			break
-		}
-		b = b[n:]
-		copy(b, b[1:])
-		b = b[:len(b)-1]
-		bSize--
-	}
-	dst = dst[:bSize]
-
-	// remove /foo/../ pargs
-	b = dst
-	for {
-		n := bytes.Index(b, strSlashDotDotSlash)
-		if n < 0 {
-			break
-		}
-		nn := bytes.LastIndexByte(b[:n], '/')
-		if nn < 0 {
-			nn = 0
-		}
-		n += len(strSlashDotDotSlash) - 1
-		copy(b[nn:], b[n:])
-		b = b[:len(b)-n+nn]
-	}
-
-	// remove /./ parts
-	for {
-		n := bytes.Index(b, strSlashDotSlash)
-		if n < 0 {
-			break
-		}
-		nn := n + len(strSlashDotSlash) - 1
-		copy(b[n:], b[nn:])
-		b = b[:len(b)-nn+n]
-	}
-
-	// remove trailing /foo/..
-	n := bytes.LastIndex(b, strSlashDotDot)
-	if n >= 0 && n+len(strSlashDotDot) == len(b) {
-		nn := bytes.LastIndexByte(b[:n], '/')
-		if nn < 0 {
-			return strSlash
-		}
-		b = b[:nn+1]
-	}
-
-	return b
-}
-
-// Returns query args.
+// QueryArgs returns query args.
 func (x *URI) QueryArgs() *Args {
 	x.parseQueryArgs()
 	return &x.queryArgs
