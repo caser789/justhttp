6c8ac8650fbb5c155be9234539e9b8ded1fdc5fb
Xue Jiao <jiao.xuejiao@gmail.com>
2021-07-26 21:41:15 +0800

    Support SameSite value None

--- cookie_test.go
+++ cookie_test.go
@@ -121,6 +121,29 @@ func TestCookieSameSite(t *testing.T) {
 		t.Fatalf("missing SameSite flag in cookie %q", s)
 	}
 
+	if err := c.Parse("foo=bar; samesite=none"); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if c.SameSite() != CookieSameSiteNoneMode {
+		t.Fatalf("SameSite None Mode must be set")
+	}
+	s = c.String()
+	if !strings.Contains(s, "; SameSite=None") {
+		t.Fatalf("missing SameSite flag in cookie %q", s)
+	}
+
+	if err := c.Parse("foo=bar"); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	c.SetSameSite(CookieSameSiteNoneMode)
+	s = c.String()
+	if !strings.Contains(s, "; SameSite=None") {
+		t.Fatalf("missing SameSite flag in cookie %q", s)
+	}
+	if !strings.Contains(s, "; secure") {
+		t.Fatalf("missing Secure flag in cookie %q", s)
+	}
+
 	if err := c.Parse("foo=bar"); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
@@ -333,8 +356,9 @@ func TestAppendRequestCookieBytes(t *testing.T) {
 }
 
 func testAppendRequestCookieBytes(t *testing.T, s, expectedS string) {
-	var cookies []argsKV
-	for _, ss := range strings.Split(s, "&") {
+	kvs := strings.Split(s, "&")
+	cookies := make([]argsKV, 0, len(kvs))
+	for _, ss := range kvs {
 		tmp := strings.SplitN(ss, "=", 2)
 		if len(tmp) != 2 {
 			t.Fatalf("Cannot find '=' in %q, part of %q", ss, s)
